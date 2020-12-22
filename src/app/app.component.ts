645314d9b1ef7e0b555377b47ba74ec6928e24e2
wardie93 <wardie93@msn.com>
2020-12-22 20:25:55 +0000

    Showing loading gif for 5 seconds
    Signed-off-by: wardie93 <wardie93@msn.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { delay } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -16,8 +15,9 @@ export class AppComponent {
 
     loadingTest(): void {
         this.http
-            .get('https://jsonplaceholder.typicode.com/posts')
-            .pipe(delay(3000))
+            .get(
+                'http://slowwly.robertomurray.co.uk/delay/5000/url/https://jsonplaceholder.typicode.com/posts'
+            )
             .subscribe();
     }
 }
